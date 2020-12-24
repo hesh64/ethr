2bc3c0afa3cab3040c419735e53bf067394796d1
hesh64 <59433056+hesh64@users.noreply.github.com>
2020-12-24 09:12:38 -0800

    rate limit

--- src/controllers/account.ts
+++ src/controllers/account.ts
@@ -1,12 +1,19 @@
-import { EventEmitter }                                       from 'events';
+import rateLimit                                              from 'express-rate-limit';
 import { Controller, Post, Get, Inject, Injectable, emitter } from '../frame/app';
 
+const mw = [
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+  })
+];
+
 @Injectable()
-@Controller({ path: '/account', params: [], middleware: [], mergeParams: true })
+@Controller({ path: '/account', params: [], middleware: mw, mergeParams: true })
 export class EthController {
   @Inject('EthWallet') eth;
 
-  constructor(@emitter private emitter: EventEmitter) {}
+  constructor() {}
 
   @Get('/:id', [ 'id', 'res' ])
   async get(id, res) {
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,1 +1,2 @@
 import './file.upload';
+import './account';
